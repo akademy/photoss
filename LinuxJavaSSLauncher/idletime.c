c1aa9abbab4d6a567b2cca7603002d69cadd2661
Matthew Wilcoxson <unknown>
2011-12-11 22:21:40 +0000

    File to detect idle time to launch as screensaver

---
+++ LinuxJavaSSLauncher/idletime.c
@@ -0,0 +1,27 @@
+// http://stackoverflow.com/questions/222606/detecting-keyboard-mouse-activity-in-linux
+// apt-get install libx11-dev libxss-dev
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/scrnsaver.h>
+z
+int GetIdleTime () {
+        time_t idle_time;
+        static XScreenSaverInfo *mit_info;
+        Display *display;
+        int screen;
+        mit_info = XScreenSaverAllocInfo();
+        if((display=XOpenDisplay(NULL)) == NULL) { return(-1); }
+        screen = DefaultScreen(display);
+        XScreenSaverQueryInfo(display, RootWindow(display,screen), mit_info);
+        idle_time = (mit_info->idle) / 1000;
+        XFree(mit_info);
+        XCloseDisplay(display); 
+        return idle_time;
+}
+
+int main() {
+        	printf("%d\n", GetIdleTime());
+}
